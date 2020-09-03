62ddfd8510ef90b5f017d1e73d6b2618236d210d
gabrieltsferreira <gabrieltsferreira@hotmail.com>
2020-09-03 19:06:22 -0300

    initial commit

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBBV0eixM5nANnOLcMLfy9TBG__16FyVnU",
+    authDomain: "mutirao-b6cff.firebaseapp.com",
+    databaseURL: "https://mutirao-b6cff.firebaseio.com",
+    projectId: "mutirao-b6cff",
+    storageBucket: "mutirao-b6cff.appspot.com",
+    messagingSenderId: "143828329064",
+    appId: "1:143828329064:web:fc45b6db04e87a728ad1dd",
+    measurementId: "G-M7MQ58KMPE"
+}
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <v-col class="text-center" cols="12" sm="4">
+            <div class="my-2">
+                <v-btn color="primary">Conectar com Google</v-btn>
+            </div>
+        </v-col>
+    </v-app>
+</template>
+
+
+
+<script>
+export default {
+
+}
+</script>
