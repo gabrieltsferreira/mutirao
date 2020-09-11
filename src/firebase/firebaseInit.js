ca042243851181d523520b4afa55b3bb89fa4ddf
gabrieltsferreira <gabrieltsferreira@hotmail.com>
2020-09-11 18:18:35 -0300

    firebase google sign in

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,7 +1,8 @@
-import firebase from 'firebase';
+import * as firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 import firebaseConfig from './firebaseConfig';
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export default firebaseApp.firestore();
+export default firebase;
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,18 +1,45 @@
 <template>
-    <v-card>
+    <v-card
+        class="mx-auto"
+        height="300"
+        width="300"
+    >  
         <v-col class="text-center" cols="12" sm="12">
-            <v-btn color="primary" @click="GoogleSignIn">Conectar com Google</v-btn>
+            <v-btn 
+                dark
+                color="#DB4437"  
+                @click="GoogleSignIn"
+            >
+                <v-icon left></v-icon>
+                Conectar com Google
+            </v-btn>
         </v-col>
     </v-card>
 </template>
 
 
-
 <script>
+import firebase from '../firebase/firebaseInit';
+
 export default {
     methods: {
         GoogleSignIn(){
-            this.$router.push('/groupOptions')
+            var provider = new firebase.auth.GoogleAuthProvider();
+
+            firebase.auth().signInWithPopup(provider)
+                .then((result) => {
+                    var token = result.credential.accessToken;
+                    var user = result.user;
+                    console.log(user);
+                    this.$router.push('/groupOptions');
+                })
+                .catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    var email = error.email;
+                    var credential = error.credential;
+                });
+
         }
     }
 }
